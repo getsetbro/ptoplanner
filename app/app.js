098768e5da80b26288a5f2669c61f7b4f921e11c
Seth Broweleit <setbro@gmail.com>
2015-08-19 12:05:32 -0500

    windows api notify

--- app/app.js
+++ app/app.js
@@ -116,4 +116,26 @@ app.run(function($rootScope) {
       $fromDP.datepicker("option", "maxDate", "12/31/"+curYear);
     };
 
+
+    // Namespace: Windows.UI.Notifications
+    if (typeof Windows !== 'undefined' && typeof Windows.UI !== 'undefined' && typeof Windows.UI.Notifications !== 'undefined') {
+
+        var notifications = Windows.UI.Notifications,
+        tile = notifications.TileTemplateType.tileSquare150x150PeekImageAndText01,
+        tileContent = notifications.TileUpdateManager.getTemplateContent(tile),
+        tileText = tileContent.getElementsByTagName('text'),
+        tileImage = tileContent.getElementsByTagName('image');
+
+        tileText[0].appendChild(tileContent.createTextNode('Demo Message'));
+        tileImage[0].setAttribute('src', 'https://unsplash.it/150/150/?random');
+        tileImage[0].setAttribute('alt', 'Random demo image');
+
+        var tileNotification = new notifications.TileNotification(tileContent);
+        var currentTime = new Date();
+        tileNotification.expirationTime = new Date(currentTime.getTime() + 600 * 1000);
+        notifications.TileUpdateManager.createTileUpdaterForApplication().update(tileNotification);
+    } else {
+        //Alternative behavior
+    }
+
 });
